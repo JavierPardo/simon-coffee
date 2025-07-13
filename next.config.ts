03676156d2c329f83a2bd7750e0f778d59233014
Javier Pardo <javier.p.g1989@gmail.com>
2025-07-12 21:18:56 -0400

    fix configuration

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/simon-coffee', // <-- Asegúrate de que esto coincida con el nombre de tu repositorio
   images: {
     unoptimized: true,
-  }
+  },
 };
 
 export default nextConfig;
